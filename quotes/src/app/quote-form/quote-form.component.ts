a68ad3727a9e2284f06e359c3c34bc5ac01b365d
melisajuma <Melisaakinyi95@gmail.com>
2019-07-09 15:00:42 +0300

    adding structure to the html file

--- quotes/src/app/quote-form/quote-form.component.ts
+++ quotes/src/app/quote-form/quote-form.component.ts
@@ -6,14 +6,14 @@ import { Component, OnInit ,Output, EventEmitter} from '@angular/core';
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
 })
-export class QuoteFormComponent implements OnInit {>>
+export class QuoteFormComponent implements OnInit {
   newQuote = new Quote (0, "", "", "", new Date(),0,0);
   @Output() addQuote = new EventEmitter<Quote>();
 
 
   submitQuote(){
     this.addQuote.emit(this.newQuote);
-    this.newQuote = new Quote (0, "", "", "", new Date(),0,0);
+    this.newQuote = new Quote (0, "", "", "", new Date(),0,0);>>
   }
   constructor() { }
 
